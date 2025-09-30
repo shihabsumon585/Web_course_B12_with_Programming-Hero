b001fb5ab16dee7ae244450db53d737466ae96fa
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-09-30 10:28:17 +0600

    daisy pricing card

---
+++ Milestone-08/Module-42/Tailwind_Axios_Rechart_awesome_components/src/components/DaisyPricingCard/DaisyPricingCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const DaisyPricingCard = ({ data }) => {
+
+    const { name, price, description, features, popular } = data;
+
+    return (
+        <div className="card w-96 bg-base-100 shadow-sm flex flex-col border-2 !bg-amber-100 text-black">
+            <div className="card-body">
+                {popular && <span className="badge badge-xs badge-warning">Most Popular</span>}
+                <div className="flex justify-between">
+                    <h2 className="text-3xl font-bold">{name}</h2>
+                    <span className="text-sm">{price}</span>
+                </div>
+                <div className='mt-3'>
+                    <p className='font-semibold'>{description}</p>
+                </div>
+                <ul className="mt-6 flex flex-col gap-2 text-xs flex-1">
+                    {
+                        features.map(feature =>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                <span>{feature}</span>
+                            </li>
+                        )
+                    }
+                </ul>
+                <div className="mt-6">
+                    <button className="btn btn-primary btn-block">Subscribe</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DaisyPricingCard;
