b56fc1c09953e73cbe55e4313804475aa40f284b
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-10-02 15:42:43 +0600

    router intial

--- Milestone-08/Module-43/react-router/src/main.jsx
+++ Milestone-08/Module-43/react-router/src/main.jsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <div className='text-center'>Welcome my the Home Page</div>
+  },
+  {
+    path: "about",
+    element: <div>Here is the about section in the company</div>
+  },
+  {
+    path: "blogs",
+    element: <h1>BLOGS here</h1>
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
