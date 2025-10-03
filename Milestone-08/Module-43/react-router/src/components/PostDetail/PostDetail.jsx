a4ba13393afb4ad0fc1156a7dd02a0da4657054c
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-10-03 21:19:49 +0600

    navigate in react router

--- Milestone-08/Module-43/react-router/src/components/PostDetail/PostDetail.jsx
+++ Milestone-08/Module-43/react-router/src/components/PostDetail/PostDetail.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetail = () => {
+    const navigate = useNavigate();
+
     const post = useLoaderData();
+
     return (
         <div>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
+            <button onClick={() => navigate(-1)}>Go Back</button>
         </div>
     );
 };
