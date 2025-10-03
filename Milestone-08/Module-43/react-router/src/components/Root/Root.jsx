9827c4ee184c66a5015f627cd75ff39419e3be66
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-10-03 21:41:10 +0600

    useNavigation complete

--- Milestone-08/Module-43/react-router/src/components/Root/Root.jsx
+++ Milestone-08/Module-43/react-router/src/components/Root/Root.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import Header from '../Header.jsx/Header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 import Sidebar from '../Sidebar/Sidebar';
 import "./Root.css"
 
 const Root = () => {
+
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
+
     return (
         <div>
             <Header></Header>
 
             <div className='root-main'>
                 <Sidebar></Sidebar>
+                {isNavigating && <span>Loading...</span>}
                 <Outlet></Outlet>
             </div>
 
