43bec87bad69a340892eda02b4621afc4e8e4fac
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-10-05 02:06:12 +0600

    controlled is controlling

--- Milestone-08/Module-44/react-core-concept-part-2/src/components/ControlledForm/ControlledForm.jsx
+++ Milestone-08/Module-44/react-core-concept-part-2/src/components/ControlledForm/ControlledForm.jsx
@@ -3,13 +3,27 @@ import React, { useState } from 'react';
 const ControlledForm = () => {
 
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const handleControlled = (e) => {
         e.preventDefault();
+        if(password.length < 6) {
+            setError("password must be 6 character or longer");
+        } else {
+            setError("");
+        }
     }
 
     const handleControlledOnChange = e => {
+        setPassword(e.target.value);
         console.log(e.target.value);
+        console.log(password.length);
+
+        // if(password.length < 6) {
+        //     setError("password must be 6 character or longer");
+        // } else {
+        //     setError("");
+        // }
     }
 
     return (
@@ -17,10 +31,13 @@ const ControlledForm = () => {
             <form onSubmit={handleControlled}>
                 <input type="text" name="name" placeholder='Your Name' />
                 <br />
-                <input type="email" onChange={handleControlledOnChange} defaultValue={password} name="email" placeholder='Your Email' />
+                <input type="password" onChange={handleControlledOnChange} defaultValue={password} name="email" placeholder='Password' />
                 <br /> <br />
                 <input  type="submit" value="Submit" />
             </form>
+            <p style={{color: "red"}}>
+                <small>{error}</small>
+            </p>
         </div>
     );
 };
