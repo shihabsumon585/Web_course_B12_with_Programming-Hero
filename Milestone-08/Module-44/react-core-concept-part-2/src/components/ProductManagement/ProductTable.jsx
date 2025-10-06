76ddf942ac6ae4c6e9a5da462c3ab5710550e007
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-10-06 10:00:07 +0600

    product table making

--- Milestone-08/Module-44/react-core-concept-part-2/src/components/ProductManagement/ProductTable.jsx
+++ Milestone-08/Module-44/react-core-concept-part-2/src/components/ProductManagement/ProductTable.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const ProductTable = ({products}) => {
+const ProductTable = ({ products }) => {
+    console.log(products);
 
     return (
         <div>
             <h3>Products: {products.length}</h3>
+            <table style={{textAlign: 'center', margin: "0 auto"}}>
+                <thead>
+                    <tr>
+                        <td>No.</td>
+                        <td>Product</td>
+                        <td>Price</td>
+                        <td>Quantity</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{product.name}</td>
+                        <td>{product.price}</td>
+                        <td>{product.quantity}</td>
+                    </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
